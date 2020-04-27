a8fbd51fdbd5c2b97c31a6c61c5e21f63248af23
dingwenquan-word <57206185+dingwenquan-word@users.noreply.github.com>
2020-04-27 19:36:19 +0800

    Delete .escheduler_env.sh

--- .escheduler_env.sh
+++
@@ -1,9 +0,0 @@
-export HADOOP_HOME=/opt/apache/hadoop/hadoop-2.6.0
-export HADOOP_CONF_DIR=/opt/apache/hadoop/hadoop-2.6.0/etc/hadoop
-export SPARK_HOME1=/opt/apache/spark/spark-2.2.0-bin-hadoop2.6
-export SPARK_HOME2=/opt/apache/spark/spark-2.2.0-bin-hadoop2.6
-export PYTHON_HOME=/opt/soft/python
-export JAVA_HOME=/usr/java/jdk1.8.0_181-cloudera
-export HIVE_HOME=/opt/apache/hive/apache-hive-2.1.1-bin
-	
-export PATH=$HADOOP_HOME/bin:$SPARK_HOME1/bin:$SPARK_HOME2/bin:$PYTHON_HOME:$JAVA_HOME/bin:$HIVE_HOME/bin:$PATH
